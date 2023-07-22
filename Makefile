2226dcb02ccde3ac761c603a2000170e53409723
Paweł Banach <1054126+pawlobanano@users.noreply.github.com>
2023-07-22 22:58:59 +0200

    chore: refinement

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
-simple-factory:
-	go build -o simple-factory ./creational/simple-factory && ./simple-factory
-
 abstract-factory:
 	go build -o abstract-factory ./creational/abstract-factory && ./abstract-factory
 
-.PHONY: simple-factory abstract-factory
+simple-factory:
+	go build -o simple-factory ./creational/simple-factory && ./simple-factory
+
+.PHONY: abstract-factory simple-factory
--- creational/abstract-factory/main.go
+++ creational/abstract-factory/main.go
@@ -4,20 +4,25 @@ import "fmt"
 
 func main() {
 	adadisFactory, _ := SportsFactory("adadis")
+	nekiFactory, _ := SportsFactory("neki")
 
 	adadisShoe := adadisFactory.makeShoe()
 	adadisShirt := adadisFactory.makeShirt()
+	nekiShoe := nekiFactory.makeShoe()
+	nekiShirt := nekiFactory.makeShirt()
 
 	printShoeDetails(adadisShoe)
 	printShirtDetails(adadisShirt)
+	printShoeDetails(nekiShoe)
+	printShirtDetails(nekiShirt)
 }
 
 func printShoeDetails(s Shoer) {
 	fmt.Printf("Shoe logo: %s\n", s.logo())
-	fmt.Printf("Shoe size: %d\n", s.size())
+	fmt.Printf("Shoe size: %d\n\n", s.size())
 }
 
 func printShirtDetails(s Shirter) {
 	fmt.Printf("Shirt logo: %s\n", s.logo())
-	fmt.Printf("Shirt size: %d\n", s.size())
+	fmt.Printf("Shirt size: %d\n\n", s.size())
 }
